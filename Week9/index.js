0ba7f1ae318d7bd31a7979dce2beb0f808b73279
Ching Keziah <chingkeziah@gmail.com>
2023-12-16 17:11:01 -0500

    Started learning javascript

---
+++ Week9/index.js
@@ -0,0 +1,27 @@
+/* javascript is one of the most popular and widely used programing and scripting language in the world right now
+it is commonly used to create interactive effects within web browsers. it was originally designed
+to run only on browser. however, in 2009, Ryan Doll took the open source javascript engine in chrome and 
+imbedded it inside a c++ program called node. with the help of node, we can now run javascript code outside of 
+a browser.
+Ecmascript is a specification which defines standards for scripting languages while javascript is a programing 
+language which confirms to this specification
+*/
+console.log('hello world');
+// this refers to a statement which is a piece of code that expresses an action to be carried out and it must be terminated by a semi colon(;)
+// what we have in the single quotes is called a string which is a sequence of characters
+
+// variables
+// variables are used to store data temporally in a computers memory and give that memory location a name
+// before es6 we used var keyword to declare a variable but going forward from es6, the best practice is to use the let keyword to declare a variable
+let name = 'keziah';
+console.log(name);
+// by default, variables in javascript are undefined
+/* rules for naming variables
+- you cannot use a reserved keyword for example; let, if, else
+- they should be meaningful. what is the purpose of the variable?
+- they cannot start with a number e.g, 1name;
+- they cannot contain a space or hyphen( -)
+- if you want to declare multiple variables, you can declare them on one line and use commas to separate them
+   but the modern best practice is to declare each variable on a single line.
+*/
+
