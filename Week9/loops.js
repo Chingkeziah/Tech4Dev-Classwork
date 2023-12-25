4ebd0556182b7869fdf302ec8474ea89d9bd8db1
Ching Keziah <chingkeziah@gmail.com>
2023-12-24 23:35:43 -0500

    understanding loops

---
+++ Week9/loops.js
@@ -0,0 +1,17 @@
+// a loop is a programming construct that executes a piece of code as long as a certain condition is met
+// We have entry control loops like the for and while loop where the test condition is checked before executing the loop
+// we also have exit control loops where the test condition is checked only after the loop has been executed eg a do while loop
+
+let age = 0;
+for(age=0;age>24;x++) 
+console.log(age);
+
+
+
+let x = 1;
+while(x<=5){
+    console.log(x)
+    x++
+}
+
+
