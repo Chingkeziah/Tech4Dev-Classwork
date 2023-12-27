cb638fc0d6b3c0d7746838a4b07bc06fb307d0e4
Ching Keziah <chingkeziah@gmail.com>
2023-12-26 23:10:51 -0500

    JavaScript operators, 90% complete

--- Week9/loops.js
+++ Week9/loops.js
@@ -2,16 +2,17 @@
 // We have entry control loops like the for and while loop where the test condition is checked before executing the loop
 // we also have exit control loops where the test condition is checked only after the loop has been executed eg a do while loop
 
-let age = 0;
-for(age=0;age>24;x++) 
-console.log(age);
+let x = 0;
+for(x=0;x<10;x++) 
+console.log(x);
 
 
 
-let x = 1;
-while(x<=5){
-    console.log(x)
-    x++
+let y = 1;
+while(y<=5){
+    console.log(y)
+    y++
 }
+
 
 
--- Week9/operators.js
+++ Week9/operators.js
@@ -1,4 +1,120 @@
 // operators in Javascript
+// these are symbols used to perform operations on operands.
+// operands refer to the values and variables 
+// for example 
+10 + 4;
+// 10 and 4 are operands while + is an operator
+
+// lists of operators
+
+// Arithmetic operators
+// here are multiple arithmetic operators these are;
+// addition
+let sum = 7 + 9
+console.log(sum);
+
+// substraction
+let y = 5 - 7
+console.log(y);
+
+// multiplication
+let x = 8 * 3
+console.log(x);
+
+// division
+let f = 12 / 3
+console.log(f);
+
+// modulus or remainder
+let j = 17 % 4;
+console.log(j); 
+
+// exponentiation
+let n = 2 ** 4 //2x2x2x2
+console.log(n); 
+
+// Assignment Operators
+// these are used to assign values to variables
+//  = simple assignment
+let v = 5;
+// =+ addition assignment
+v += 3;
+//  -= subtraction assignment
+v -= 3;
+//  *= multiplication assignment
+v *= 3;
+//  /= division assignment
+v /= 3;
+// modulus assignment
+v %= 3;
+// exponentiation assignment
+v **= 3;
+ 
+// Increment and decrement operators
+// The increment and decrement operators are used to increase or decrease the value of a variable by 1
+// The increment operator is ++
+// The decrement operator is --
+// Both operators can be used in two ways which is prefix and postfix
+// Prefix increment and decrement operator
+
+// prefix increment
+let b = 10;
+console.log(++b);
+console.log(b);
+// here the value of b is incremented by one
+// the new value of b is printed
+// you'll see that the operator is displayed before the value
+
+// prefix decrement
+console.log(--b);
+console.log(b);
+
+// Postfix increment and decrement operator
+// postfix increment operator
+/* here the operator is added after the variable and the value of the variable is  used before 
+it is incremented. That is, the current value of a is printed before it's new value*/
+let a = 10;
+console.log(a++);
+console.log(a);
+
+// postfix decrement operator
+console.log(a--);
+console.log(a);
+
+// comparison operators
+// these operators compare two values and return a boolean vaue of either true or false
+// can be used in decision making and loop programing in js
+// < (less than)
+
+let z = 5;
+let c = 10;
+console.log(z<c);
+
+// > (greater than)
+console.log(z>c);
+
+// <= (less than or equal to)
+console.log(z<=c);
+
+// >= (greater than or equal to)
+console.log(z>=c);
+
+// == (equal checks , checks the equality of two values)
+
+// != (inequality) (not equal) (flipped value of equal checks)
+// === (strict equality checks) (checks the data type too)
+// !== (strict inequality (!==) (flipped value strict inequality check)
+
+
+
+
+
+
+
+
+
+
+
 // + or concatenation
 2 + 2 
 2 + '2'
